7b7c27d0efa8041358d34155b93f6e3aab897531
GummyGod <gummy.pwnn@yahoo.com>
2019-03-12 17:13:22 +0200

    update post and comment mutations

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -91,7 +91,27 @@ const Mutation = {
         return deletedPosts[0];
 
     },
-    createComment(parent,args,{db},info) {
+    updatePost(parent,args,{ db },info) {
+        const {id, data} = args;
+        const post = db.posts.find(post => post.id === id);
+
+        if(!post) throw new Error('Post not found');
+
+        if(typeof data.title === 'string') {
+            post.title = data.title;
+        }
+
+        if(typeof data.body === 'string') {
+            post.body = data.body;
+        };
+
+        if (typeof data.published === 'boolean') {
+            post.published = data.published;
+        }
+
+        return post;
+    },
+    createComment(parent,args,{ db },info) {
         const userExists = db.users.some((user) => user.id === args.data.author);
         const postExists = db.posts.some((post) => post.id === args.data.post && post.published);
 
@@ -115,6 +135,18 @@ const Mutation = {
         const deletedComment = db.comments.splice(commentIndex, 1);
 
         return deletedComment[0];
+    },
+    updateComment(parent,args,{ db },info) {
+        const { id , data } = args;
+        const comment = db.comments.find(comment => comment.id === id);
+
+        if(!comment) throw new Error('Comment not found');
+
+        if (typeof data.text === 'string') {
+            comment.text = data.text;
+        }
+
+        return comment;
     }
 };
 
